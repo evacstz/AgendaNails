d180295a7c812d6ddca2b46b687b9a8a95e03ae1
eva lohane <eva.cordeiro@aluno.ce.gov.br>
2025-05-27 09:20:54 -0300

    add conexão

---
+++ codigos/conexao.php
@@ -0,0 +1,10 @@
+<?php
+    try {
+        $conexao = new PDO("mysql:host=localhost; dbname=AgendaNails", "root",
+        "");
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conexao->exec("set names utf8");
+    } catch (PDOException $erro) {
+        echo "Erro na conexão:" . $erro->getMessage();
+    }
+?>
