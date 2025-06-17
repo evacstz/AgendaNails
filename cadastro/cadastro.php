64711eeda536a3e28d92b7f208ce18008271f389
eva lohane <eva.cordeiro@aluno.ce.gov.br>
2025-06-17 09:06:49 -0300

    att no cadastro, não finalizado
    Consegui cadastrar!!!

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -12,4 +12,20 @@
     if(empty($nome) || empty($email) || empty($senha)) {
         exit("Por favor, preencha todos os campos");
     }
+
+    if(isset($_POST['nome-completo']) && isset($_POST['email']) && isset($_POST['senha'])) {
+        try {
+            $consulta = $conexao->prepare("INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)");
+            $consulta->bindValue(":nome", $nome);
+            $consulta->bindValue(":email", $email);
+            $consulta->bindValue(":senha", md5($senha));
+            $consulta->execute();
+        } catch (PDOException $erro) { 
+            echo "Erro: " . $erro->getMessage(); 
+        }
+    } else {
+        echo "Erro ao tentar se cadastrar.s";
+    }
+
+    
 ?>
