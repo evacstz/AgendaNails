11c840ba651915713f62c781667f806d002635b3
eva lohane <eva.cordeiro@aluno.ce.gov.br>
2025-06-24 20:26:12 -0300

    Adicionando páginas de cadastro de serviços

---
+++ cadastro-servicos/pagina-servicos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AgendaNails</title>
+</head>
+<body>
+    <div>
+        <a href="cadastrar-servico.php">CADASTRAR NOVO SERVIÇO</a>
+    </div>
+</body>
+</html>
