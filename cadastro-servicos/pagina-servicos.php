566ae77eebc9436adac37f7cb6e76c9175dd2b1f
eva lohane <eva.cordeiro@aluno.ce.gov.br>
2025-06-25 12:43:20 -0300

    Atualizando

--- cadastro-servicos/pagina-servicos.php
+++ cadastro-servicos/pagina-servicos.php
@@ -4,10 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AgendaNails</title>
+    <link rel="stylesheet" href="../assets/css/base-style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div>
-        <a href="cadastrar-servico.php">CADASTRAR NOVO SERVIÇO</a>
+    <div class="div-pai">
+        <div class="menu-lateral">
+            <div class="agenda-nails">AgendaNails</div>
+                <div class="menu">
+                    <a href="../pagina-inicial.php" class="abas-menu">Página inicial</a>
+                    <a class="abas-menu">Clientes</a>
+                    <a class="abas-menu">Serviços</a>
+                    <a class="abas-menu">Agendamentos</a>
+                    <a class="abas-menu">Financeiro</a>
+                </div>
+            <div class="botao-sair-div">
+                <a href="../login/logout.php" class="botao-sair">Sair</a>
+            </div>
+        </div>
+
+        <div id="botao-cadastrar">
+            <a href="cadastrar-servico.php">CADASTRAR NOVO SERVIÇO</a>
+        </div>
+
+        <div class="barra-superior">
+            <h3 class="mensagem">SERVIÇOS</h3>
+        </div>
     </div>
 </body>
 </html>
