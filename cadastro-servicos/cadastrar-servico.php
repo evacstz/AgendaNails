f46330b1e0272462caa0f019e2d2a87de8ed1463
eva lohane <eva.cordeiro@aluno.ce.gov.br>
2025-06-25 21:16:15 -0300

    Finalizando cadastro de serviço

--- cadastro-servicos/cadastrar-servico.php
+++ cadastro-servicos/cadastrar-servico.php
@@ -1,3 +1,33 @@
 <?php
-echo 'voce vheggou ate auqi';
+    require_once '../conexao.php';
+
+    if (!isset($_POST['nome-servico']) || !isset($_POST['preco'])) {
+        exit('Não foi possível cadastrar um novo serviço.');
+    }
+
+    $servico = trim($_POST['nome-servico']);
+    $preco = trim($_POST['preco']);
+
+    if (empty($servico) || empty($preco)) {
+        exit("Por favor, preencha todos os campos");
+    }
+
+    try {
+        $consulta_verifica = $conexao->prepare("SELECT * FROM servicos WHERE nome_servico = :servico");
+        $consulta_verifica->bindValue(":servico", $servico);
+        $consulta_verifica->execute();
+
+        if ($consulta_verifica->rowCount() > 0) {
+            header("Location: pagina-servicos.php?erro=3");
+            exit;
+        } else {
+            $consulta_insere= $conexao->prepare("INSERT INTO servicos (nome_servico, preco) VALUES (:servico, :preco)");
+            $consulta_insere->bindValue(":servico", $servico);
+            $consulta_insere->bindValue(":preco", $preco);
+            $consulta_insere->execute();
+                header("Location: pagina-servicos.php");
+        }
+    } catch (PDOException $erro) { 
+        echo "Erro ao tentar cadastrar serviço: " . $erro->getMessage(); 
+    }
 ?>
--- cadastro-servicos/pagina-servicos.php
+++ cadastro-servicos/pagina-servicos.php
@@ -23,12 +23,36 @@
             </div>
         </div>
 
-        <div id="botao-cadastrar">
-            <a href="cadastrar-servico.php">CADASTRAR NOVO SERVIÇO</a>
-        </div>
+        <div class="conteudo-central">
+            <div class="barra-superior">
+                <h3 class="mensagem">SERVIÇOS</h3>
+            </div>
 
-        <div class="barra-superior">
-            <h3 class="mensagem">SERVIÇOS</h3>
+            <div id="texto-cadastrar">
+                <h5>CADASTRAR NOVO SERVIÇO</h5>
+            </div>
+
+            <form action="cadastrar-servico.php" method="POST" id="cadastrar-servico">
+                <div class="grupo-inputs">
+                    <label for="nome-servico" class="rotulos">Serviço:</label>
+                    <input type="text" name="nome-servico" class="input-nome-preco" required>
+
+                    <label for="preco" class="rotulos">Preço:</label>
+                    <input type="text" name="preco" step="0.01" class="input-nome-preco" required>
+                </div>
+
+                <div class="div-botao">
+                    <input type="submit" name="cadastrar" class="botao-servico" value="CADASTRAR SERVIÇO">
+                </div>
+            </form>
+            
+            <div class="servico-cadastrado">
+            <?php
+                if (isset($_GET['erro']) && $_GET['erro'] == 3) {
+                    echo '<p>Este serviço já está cadastrado.</p>';
+                }
+            ?>
+        </div>
         </div>
     </div>
 </body>
